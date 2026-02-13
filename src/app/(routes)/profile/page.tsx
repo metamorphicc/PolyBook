2709493b92972af272d34f764c4960304fe25acf
sergey <artemkruglikov370@gmail.com>
2026-02-13 19:13:09 +0700

    some fixed issues

--- src/app/(routes)/profile/page.tsx
+++ src/app/(routes)/profile/page.tsx
@@ -1,25 +1,31 @@
-"use client"
-import { useAppKitAccount } from "@reown/appkit/react"
+"use client";
+import { useAppKitAccount } from "@reown/appkit/react";
 import { useAppKitBalance } from "@reown/appkit/react";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 export default function Profile() {
-    const {address, isConnected, status} = useAppKitAccount();
-    const {fetchBalance} = useAppKitBalance();
-    const [balance, setBalance] = useState<any>(null);
+  const { address, isConnected, status } = useAppKitAccount();
+  const { fetchBalance } = useAppKitBalance();
+  const [balance, setBalance] = useState<any>(null);
+    const router = useRouter()
+  useEffect(() => {
+    (async () => {
+      const balance = await fetchBalance();
+      setBalance(balance);
+    })();
+  });
+  return (
+    <>
+      <div className=" gap-4">
+        <p>{`Profile: ${address}`}</p>
 
-    useEffect(() => {
-        (async () => {
-            const balance = await fetchBalance()
-            setBalance(balance)
-        })()
-    })
-    return (<>
-        <div>
-            <p>
-                {`Profile: ${address}`}
-                {`Balance: ${JSON.stringify(balance)}`}
-            </p>
-        </div>
-    </>)
-
+        <p>{`Balance: ${JSON.stringify(balance?.data?.formatted) ?? "0"}`}</p>
+      </div>
+      <div className="mt-5">
+        <button onClick={() => router.back()} className="px-4 py-2 border cursor-pointer">
+            back
+        </button>
+      </div>
+    </>
+  );
 }
