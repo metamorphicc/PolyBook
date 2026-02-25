ffebb0d8af2bba437d95e32780b68d2e814fae61
sergey <artemkruglikov370@gmail.com>
2026-02-25 17:28:07 +0700

    I hope it will be last small comm

--- Provider.tsx
+++ Provider.tsx
@@ -2,9 +2,44 @@
 
 import { WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { wagmiAdapter } from "./src/app/AppKit/appkit";
-
+import { createAppKit } from "@reown/appkit/react";
+import {
+  mainnet,
+  arbitrum,
+  type AppKitNetwork,
+  bsc,
+  polygon,
+} from "@reown/appkit/networks";
+import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
+import "dotenv/config";
+import { ReownAuthentication } from "@reown/appkit-siwx";
 const queryClient = new QueryClient();
+
+const networks: [AppKitNetwork, ...AppKitNetwork[]] = [mainnet, polygon];
+
+const projectId = "d86a9102e9f88948ac5d809a1a6e9cad";
+
+const metadata = {
+  name: "Polybook",
+  description: "Polybook dApp",
+  url: "http://localhost:3002",
+};
+
+export const wagmiAdapter = new WagmiAdapter({
+  networks,
+  projectId,
+  ssr: true,
+});
+
+createAppKit({
+  adapters: [wagmiAdapter],
+  networks,
+  projectId,
+  features: {
+    analytics: true,
+  },
+  siwx: new ReownAuthentication(),
+});
 
 export function AppKitProviderr({ children }: { children: React.ReactNode }) {
   return (
